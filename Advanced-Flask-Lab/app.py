d7900967712e7e7334e401523d3dd152cf5b79e6
joleensh23-meet <109904191+joleensh23-meet@users.noreply.github.com>
2022-07-27 18:09:33 +0300

    Update

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -20,17 +20,21 @@ posts = {
     "https://pbs.twimg.com/media/FI_UkcnVIAAUvWN?format=jpg&name=medium": "2020 cohort in their Y1 summer!"}
 
 
+
 #####
 
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html',image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_10152506946962507_2442851309927378964_o.png?stp=c49.0.148.148a_dst-png_p148x148&_nc_cat=111&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=d-R_wjIm2d8AX_NugPv&_nc_ht=scontent.ftlv21-1.fna&oh=00_AT8CSjiG8-biys_qQ9tU-yrxzJSp6HANxsz5c3mFVk-G2Q&oe=62FD0A1B" ,user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders.")
+    return render_template('index.html',image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_10152506946962507_2442851309927378964_o.png?stp=c49.0.148.148a_dst-png_p148x148&_nc_cat=111&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=d-R_wjIm2d8AX_NugPv&_nc_ht=scontent.ftlv21-1.fna&oh=00_AT8CSjiG8-biys_qQ9tU-yrxzJSp6HANxsz5c3mFVk-G2Q&oe=62FD0A1B" ,user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders." , posts=posts)
+
 
 
 @app.route('/about')  # '/' for the default page
 def about():
     return render_template('about.html')
+
+
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
